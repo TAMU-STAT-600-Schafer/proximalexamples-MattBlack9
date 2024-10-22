0e6f83963c0a93024ebab45a3cef8e6c01d0ea0a
Matthew Blackley <123616859+MattBlack9@users.noreply.github.com>
2024-10-22 10:50:10 -0500

    Update LassoProximalExample.R

--- LassoProximalExample.R
+++ LassoProximalExample.R
@@ -48,6 +48,66 @@ fitLASSOstandardized_prox <- function(Xtilde, Ytilde, lambda, beta_start = NULL,
 }
 
 
+
+fitLASSOstandardized_prox_Nesterov <- function(Xtilde, Ytilde, lambda, beta_start = NULL, eps = 0.0001, s = 0.01){
+  # Compatibility and other checks
+  p <- ncol(Xtilde)
+  n <- nrow(Xtilde)
+  if (length(Ytilde) != n){
+    stop("Dimensions of X and Y don't match")
+  }
+  if (lambda < 0){
+    stop("Only non-negative values of lambda are allowed!")
+  }
+  if (is.null(beta_start)){
+    # Initialize beta0
+    beta_start <- rep(0,p)
+  }else if (length(beta_start) != p){
+    stop("Supplied initial starting point has length different from p!")
+  }
+  
+  # Proximal gradient-descent implementation
+  error <- 100
+  # calculate full residual
+  r <- Ytilde - Xtilde %*% beta_start
+  beta <- beta_start
+  x <- beta
+
+  lambda_t <- 0
+  lambda_t1 <- (1 + sqrt(1 + 4 * lambda_t^2)) / 2
+  
+  while(error > eps){
+    beta_old <- beta
+    x_old <- x
+    
+    # move in direction of gradient MSE
+    beta_move <- beta + s * crossprod(Xtilde, r) / n
+
+    # apply the prox operator (soft) to beta_move
+    x <- soft(beta_move, s * lambda)
+
+    # apply the Nesterov acceleration
+    beta <_ x + ((lambda_t - 1) / lambda_t1) * (x - x_old)
+
+    # update the residual
+    r <- r - Xtilde %*%(beta - beta_old)
+
+    # update lambdas
+    lambda_t <- lambda_t1
+    lambda_t1 <- (1 + sqrt(1 + 4 * lambda_t^2)) / 2
+
+    # calculate the error
+    error <- lasso(Xtilde, Ytilde, beta_old, lambda) - lasso(Xtilde, Ytilde, beta, lambda)
+    error <- abs(error)
+    }
+  
+  return(list(beta = beta, fmin = lasso(Xtilde, Ytilde, beta, lambda)))
+}
+
+
+
+
+
 # Fit LASSO on standardized data for a given lambda
 # Xtilde - centered and scaled X, n x p
 # Ytilde - centered Y, n x 1
